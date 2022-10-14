13fb9f99bda335b77c0203c9f57ee9e0c8d65ba0
Francisco Javier Alarza <fjalarza@viewnext.com>
2022-10-15 00:13:33 +0200

    Initial Commit

---
+++ MarvelApp/MarvelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelAppApp.swift
+//  MarvelApp
+//
+//  Created by Francisco Javier Alarza Sanchez on 15/10/22.
+//
+
+import SwiftUI
+
+@main
+struct MarvelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
